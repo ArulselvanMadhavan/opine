e696ce77a0e4652850ed10485aeec79db8429a3c
Arulselvan Madhavan <arulselvan1234@gmail.com>
2023-02-07 22:04:58 -0800

    Save changes

--- bin/main.ml
+++ bin/main.ml
@@ -45,7 +45,7 @@ let parse_module enable_type_comment filename =
     (*   Sexplib.Sexp.pp_hum *)
     (*   (Transformations.Stats.State.sexp_of_t s); *)
     let src = (Opine.Unparse.py_module Opine.Unparse.State.default ast).source in
-    Stdio.Out_channel.write_all "pyre.txt" ~data:src;
+    (* Stdio.Out_channel.write_all "pyre.txt" ~data:src; *)
     Format.printf "%s\n" src)
 ;;
 
@@ -58,7 +58,7 @@ let parse_expression filename =
       Parser.Concrete.parse_expression ~context content)
   in
   handle_result result ~f:(fun ast ->
-    Format.printf "%a\n" Sexplib.Sexp.pp_hum (Concrete.Expression.sexp_of_t ast);
+    (* Format.printf "%a\n" Sexplib.Sexp.pp_hum (Concrete.Expression.sexp_of_t ast); *)
     Format.printf "%s\n" (Opine.Unparse.expr Opine.Unparse.State.default ast).source)
 ;;
 
